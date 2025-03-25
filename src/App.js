a90f3fbd57d5f19ef5d583cd55552e4f69e3b6c9
Ribhash <ribhash123@gmail.com>
2025-03-25 14:07:33 +0545

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Navigation } from 'react-minimal-side-navigation';
+import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
+import { FaCoffee, FaUsers, FaInbox, FaCloud} from 'react-icons/fa';  // Import icons
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation
+        activeItemId="/management/members"
+        onSelect={({ itemId }) => {
+          // Handle item selection
+        }}
+        items={[
+          {
+            title: 'Dashboard',
+            itemId: '/dashboard',
+            elemBefore: () => <FaInbox />, // Using FaInbox icon
+          },
+          {
+            title: 'Management',
+            itemId: '/management',
+            elemBefore: () => <FaUsers />, // Using FaUsers icon
+            subNav: [
+              {
+                title: 'Projects',
+                itemId: '/management/projects',
+                elemBefore: () => <FaCloud />, // Using FaCloudSnow icon
+              },
+              {
+                title: 'Members',
+                itemId: '/management/members',
+                elemBefore: () => <FaCoffee />, // Using FaCoffee icon
+              },
+            ],
+          },
+          {
+            title: 'Another Item',
+            itemId: '/another',
+            subNav: [
+              {
+                title: 'Teams',
+                itemId: '/management/teams',
+              },
+            ],
+          },
+        ]}
+      />
+    </>
   );
-}
+};
 
 export default App;
